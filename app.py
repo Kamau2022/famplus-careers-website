55ad5ffc0b925911e6c50aa4251556f5a03da284
kamau2022 <benkabenson@gmail.com>
2023-12-16 00:12:12 +0300

    added a database

--- app.py
+++ app.py
@@ -1,34 +1,29 @@
 from flask import Flask, render_template, jsonify
+from database import load_jobs_from_db, load_job_from_db
+import json
+
 app = Flask(__name__)
 
-JOBS = [
-{
-    'id': 1,
-    'title': 'Human Resource Manager',
-    'location': 'Nairobi, Kenya',
-    'salary': 'Ksh. 140, 000'  
-},
-{
-    'id': 2,
-    'title': 'Data Analyst',
-    'location': 'Nairobi, Kenya',
-    'salary': 'Ksh. 140, 000'  
-},
-{
-    'id': 3,
-    'title': 'Software Engineer',
-    'location': 'Nairobi, Kenya'
-      
-}
 
-]
 @app.route('/')
 def hello_famplus():
-    return render_template("home.html", jobs=JOBS)
+    jobs = load_jobs_from_db() 
+    return render_template("home.html", jobs=jobs)
 
 @app.route('/api/jobs')
 def list_jobs():
-    return jsonify(JOBS)
+    jobs = load_jobs_from_db()
+    return jsonify(jobs)
+
+
+
+
+@app.route("/jobs/<id>")
+def show_job(id):
+    jobs = load_job_from_db(id)
+    if not jobs:
+        return "Not found", 404
+    return render_template("jobpage.html", jobs=jobs)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -12,6 +12,18 @@ connection_string = f'mysql+pymysql://{username}:{password}@{hostname}:{port}/{d
 # an SQLAlchemy engine
 engine = create_engine(connection_string)
 
-with engine.connect() as conn:
-    result = conn.execute(text("select * from jobs"))
-    print(result.all())
+
+def load_jobs_from_db():
+    with engine.connect() as conn:
+        result = conn.execute(text("SELECT * FROM jobs"))
+        jobs = [(row) for row in result.fetchall()]
+        return jobs
+
+def load_job_from_db(id):
+    with engine.connect() as conn:
+        result = conn.execute(text("SELECT * FROM jobs WHERE id = :val").bindparams(val=id))
+        row = result.fetchone()
+        if row is None:
+            return None
+        else:
+            return (row)
